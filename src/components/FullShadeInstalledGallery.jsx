5b059672ba64f52dd6d4e0139055be22d182f8e4
Randy <randyisbusy@gmail.com>
2022-07-24 23:08:07 -0700

    starts photo gallery file

---
+++ src/components/FullShadeInstalledGallery.jsx
@@ -0,0 +1,14 @@
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+import defaultImage from "./pictures/four-horse-stalls.jpg";
+    const ShadeGallery = [
+        {original: 
+         thumbnail: 
+        }
+    ]
+
+const FullShadeInstalledGallery=()=>{
+    return()
+}
+export default FullShadeInstalledGallery
